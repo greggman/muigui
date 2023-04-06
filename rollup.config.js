4bc0b98b58159d31a7e0073615f3fb8c5eded28f
Gregg Tavares <github@greggman.com>
2023-04-06 16:48:09 -0700

    switch from google-closure-compiler to terser
    google-closure-compiler doesn't have support for
    private fields

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import fs from 'fs';
 
 const pkg = JSON.parse(fs.readFileSync('package.json', {encoding: 'utf8'}));
@@ -23,6 +24,30 @@ export default [
       {
         format: 'es',
         file: 'dist/0.x/muigui.module.js',
+        indent: '  ',
+        banner,
+      },
+    ],
+  },
+  {
+    input: 'src/umd.js',
+    plugins: [
+      resolve({
+        modulesOnly: true,
+      }),
+      terser(),
+    ],
+    output: [
+      {
+        format: 'umd',
+        file: 'dist/0.x/muigui.min.js',
+        indent: '  ',
+        banner,
+        name: 'GUI',
+      },
+      {
+        format: 'es',
+        file: 'dist/0.x/muigui.module.min.js',
         indent: '  ',
         banner,
       },
